94c5c68a67977f62018d2ba1ec4172827ebce9fa
Peter Hoffmann <Hoffmann.P@gmx.net>
2016-05-23 16:47:42 +0200

    Sublime-Dateine hinzugefügt

--- init.sh
+++ init.sh
@@ -3,8 +3,12 @@
 if [[ ! -d ".git" ]]; then
 	git clone https://github.com/HoffmannP/skeleton.git .
 fi
-
 rm -rf .git
+
+PROJECT="$(basename $(pwd))"
+rm $0 TODO.md README.md
+rename 's/{PROJECT}/'"$PROJECT"'/' *
+
 git init
 echo "ready to code"
-rm $0 TODO.md README.md
+subl $PROJECT.sublime-project
