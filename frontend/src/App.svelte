2b44f7f8be1ceccd486f87a53e9e9fb2bf666e2a
Berengar W. Lehr <Berengar.Lehr@uni-jena.de>
2022-05-30 12:33:44 +0200

    With routing

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,13 +1,25 @@
 <script>
     import { FeatherIcon } from 'svelte-feather-icons'
+    import Router from 'svelte-spa-router'
+    import { wrap } from 'svelte-spa-router/wrap'
+
     /* global isProduction */
     export let appname
     export const name = 'World'
     const status = isProduction
+
+    const globalProp = component => wrap({ component, props: {
+        // backend = 'http://localhost:5001/api/v1'
+    }})
+  
+    const routes = {
+        '*': globalProp(NotFound)
+    }
 </script>
 
 <main>
     <h1>{appname}: Hello {name} with <FeatherIcon /> {#if !status}[dev-mode]{/if}</h1>
+    <Router/>
 </main>
 
 <style>
