fefbc9e931f835418749bd0e3ba2f95ce07710e2
Berengar W. Lehr <Berengar.Lehr@uni-jena.de>
2022-07-13 16:34:16 +0200

    Redaktionell

--- frontend/rollup.config.js
+++ frontend/rollup.config.js
@@ -32,62 +32,63 @@ function serve () {
 
 export default [
   {
-  input: 'src/main.js',
-  output: {
-    sourcemap: true,
-    format: 'iife',
-    name: 'app',
-    file: 'public/build/bundle.js'
+    input: 'src/main.js',
+    output: {
+      sourcemap: true,
+      format: 'iife',
+      name: 'app',
+      file: 'public/build/bundle.js'
+    },
+    plugins: [
+      svelte({
+        compilerOptions: {
+          // enable run-time checks when not in production
+          dev: !production
+        }
+      }),
+      // we'll extract any component CSS out into
+      // a separate file - better for performance
+      css({
+        output: 'bundle.css'
+      }),
+
+      // If you have external dependencies installed from
+      // npm, you'll most likely need these plugins. In
+      // some cases you'll need additional configuration -
+      // consult the documentation for details:
+      // https://github.com/rollup/plugins/tree/master/packages/commonjs
+      resolve({
+        browser: true,
+        dedupe: ['svelte']
+      }),
+      commonjs(),
+
+      replace({
+        // 2 level deep object should be stringify
+        // env: JSON.stringify({
+        //   isProd: production
+        // }),
+        isProduction: production,
+        preventAssignment: true
+      }),
+
+      // In dev mode, call `npm run start` once
+      // the bundle has been generated
+      !production && serve(),
+
+      // Watch the `public` directory and refresh the
+      // browser on changes when not in production
+      !production && livereload('public'),
+
+      // If we're building for production (npm run build
+      // instead of npm run dev), minify
+      production && terser()
+    ],
+    watch: {
+      clearScreen: false
+    }
   },
-  plugins: [
-    svelte({
-      compilerOptions: {
-        // enable run-time checks when not in production
-        dev: !production
-      }
-    }),
-    // we'll extract any component CSS out into
-    // a separate file - better for performance
-    css({
-      output: 'bundle.css'
-    }),
-
-    // If you have external dependencies installed from
-    // npm, you'll most likely need these plugins. In
-    // some cases you'll need additional configuration -
-    // consult the documentation for details:
-    // https://github.com/rollup/plugins/tree/master/packages/commonjs
-    resolve({
-      browser: true,
-      dedupe: ['svelte']
-    }),
-    commonjs(),
-
-    replace({
-      // 2 level deep object should be stringify
-      // env: JSON.stringify({
-      //   isProd: production
-      // }),
-      isProduction: production,
-      preventAssignment: true
-    }),
-
-    // In dev mode, call `npm run start` once
-    // the bundle has been generated
-    !production && serve(),
-
-    // Watch the `public` directory and refresh the
-    // browser on changes when not in production
-    !production && livereload('public'),
-
-    // If we're building for production (npm run build
-    // instead of npm run dev), minify
-    production && terser()
-  ],
-  watch: {
-    clearScreen: false
-  }
-  }, {
+  {
     input: 'public/*.html',
     output: { dir: 'public/build/' },
     plugins: [
@@ -96,4 +97,5 @@ export default [
         publicPath: '/logsearcher'
       })
     ]
-}
+  }
+]
