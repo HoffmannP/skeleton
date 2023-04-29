ffc4f46c70ca991d6fb08793aa3a8994c19b41d0
Berengar W. Lehr <Berengar.Lehr@uni-jena.de>
2023-04-29 08:18:52 +0200

    Better install and update

--- install.sh
+++ install.sh
@@ -2,10 +2,18 @@
 
 (
     cd frontend
-    yarn install
+    npm install
 )
 (
     cd backend
     pip install -r requirements.txt
 )
 git remote remove origin
+read -p "Namespace: (default)" NAMESPACE
+
+if [[ -n "$NAMESPACE" ]]
+then
+    NAMESPACE=default
+else
+    sed -i 's/NAMESPACE=default/NAMESPACE='"$NAMESPACE"'/' update.sh
+fi
