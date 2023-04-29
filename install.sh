87dbdb9a889ee8b7a1010bf4e90fdf95cdb8c0de
Berengar W. Lehr <Berengar.Lehr@uni-jena.de>
2023-04-29 08:42:37 +0200

    Even better install

--- install.sh
+++ install.sh
@@ -1,19 +1,41 @@
 #!/bin/bash
 
-(
+function reset_git () {
+  rm -rf .git
+
+  git init
+  git add -A
+  git commit -m 'Init'
+
+  read -p "Git-Repository: " GITURL
+  echo $GITURL
+  git remote add origin $GITURL
+  git push --set-upstream origin main
+}
+
+function set_namespace () {
+  read -p "Namespace: (default) " NAMESPACE
+
+  if [[ -z "$NAMESPACE" ]]
+  then
+    NAMESPACE="default"
+  fi
+
+  echo $NAMESPACE
+  sed -i '/export NAMESPACE/s/=".*$/="'"$NAMESPACE"'"/' update.sh
+}
+
+function install_all () {
+  (
     cd frontend
     npm install
-)
-(
+  )
+  (
     cd backend
     pip install -r requirements.txt
-)
-git remote remove origin
-read -p "Namespace: (default)" NAMESPACE
+  )
+}
 
-if [[ -n "$NAMESPACE" ]]
-then
-    NAMESPACE=default
-else
-    sed -i 's/NAMESPACE=default/NAMESPACE='"$NAMESPACE"'/' update.sh
-fi
+install_all
+set_namespace
+reset_git
