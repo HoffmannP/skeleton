b1f06689beec1c8ee7c7d9ad8829c22d460b0d29
Berengar W. Lehr <Berengar.Lehr@uni-jena.de>
2023-05-02 11:36:16 +0200

    Update

--- update.sh
+++ update.sh
@@ -2,16 +2,16 @@
 
 set -e
 
-SERVER="macberengar.rz.uni-jena.de:5000"
-NAME="##############"
-SERVER_NAME=$SERVER/$NAME
+export SERVER_NAME=$(git remote get-url origin | cut -d@ -f2 | sed -r 's/:/:5050\//;s/.git//')
+export NAME=$(echo $SERVER_NAME | rev | cut -d\/ -f1 | rev)
+export NAMESPACE="default"
+KUBECTL="rancher kubectl --insecure-skip-tls-verify"
 
 if [[ "$1" != "--nobuild" ]]
 then
     (
         cd frontend
-        rm public/build/assets/*
-        yarn build
+        npm run build
     )
     docker build -t "${SERVER_NAME}:latest" .
 else
@@ -23,20 +23,12 @@ docker tag "$SERVER_NAME:latest" "$SERVER_NAME:$VERSION"
 docker push "$SERVER_NAME:latest"
 docker push "$SERVER_NAME:$VERSION"
 
-if [[ "$1" == "--deploy" ]]
+if [[ -f "deploy.yaml" ]]
 then
-    http --auth "token-zwksm:$(cat secret.key)" \
-        POST "https://rancher.rz.uni-jena.de/v3/projects/c-t8sln:p-k7zzr/workload" <<EOJ
-{"type":"workload",
-"namespaceId":"default",
-"containers":[{
-    "type":"container",
-    "image":"$SERVER_NAME:$VERSION",
-    "ports":[{"containerPort":80,"hostPort":0,"type":"containerPort","kind":"NodePort","protocol":"TCP","name":null}],
-    "name":"$NAME"
-"name":"$NAME"}
-EOJ
+    $KUBECTL -n $NAMESPACE rollout restart deploy $NAME
+    echo "Hallo"
 else
-    http --auth "token-zwksm:$(cat secret.key)" \
-        POST "https://rancher.rz.uni-jena.de/v3/project/c-kl5lw:p-h5jvn/workloads/deployment:default:"$NAME"?action=redeploy"
+    cat deploy.yaml.template | envsubst > deploy.yaml
+    rm deploy.yaml.template
+    $KUBECTL -n $NAMESPACE apply -f deploy.yaml
 fi
